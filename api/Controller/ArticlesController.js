2b7b80d2f5d787c9c7d8e6ba95cf9ebd8b44557d
Peter Huang <miterfrnats@gmail.com>
2022-11-11 16:12:18 +0800

    refactor: remove unnecessary import

--- api/Controller/ArticlesController.js
+++ api/Controller/ArticlesController.js
@@ -1,6 +1,5 @@
 import { dbContext } from "../Models/ORM/DbContext.js";
 import { Article } from '../Models/ORM/Article.js';
-import { User } from '../Models/ORM/User.js';
 
 export const ArticlesController  = () => {
     return dbContext.sync().then(()=>{
